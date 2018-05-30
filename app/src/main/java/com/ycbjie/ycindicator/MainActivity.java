a4c71efc812c8642752e7c38ccbbb6504b369d17
yangchong211 <yangchong211@163.com>
2018-05-30 09:43:11 +0800

    YCIndicator

---
+++ app/src/main/java/com/ycbjie/ycindicator/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ycbjie.ycindicator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
